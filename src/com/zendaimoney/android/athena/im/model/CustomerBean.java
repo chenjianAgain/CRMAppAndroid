ad77b39b0dc8e0171ca2da2f52fded1bba9cc5b7
annru <annnru@163.com>
2014-08-21 17:00:26 +0800

    储备客户bean数据模型

---
+++ src/com/zendaimoney/android/athena/im/model/CustomerBean.java
@@ -0,0 +1,21 @@
+package com.zendaimoney.android.athena.im.model;
+
+public class CustomerBean {
+	private String name;
+	private String group;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	
+
+}
